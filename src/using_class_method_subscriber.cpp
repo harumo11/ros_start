7f803431cf26aa039db00e781acb9f2621112e69
harumo11 <s_harumo@hotmail.co.jp>
2021-09-14 12:25:14 +0900

    update loging method to stream

--- src/using_class_method_subscriber.cpp
+++ src/using_class_method_subscriber.cpp
@@ -3,34 +3,33 @@
 #include <std_msgs/String.h>
 
 class Listener {
-	public:
-		//valiable
-		std::string receive_data;
+  public:
+    // valiable
+    std::string receive_data;
 
-		//function
-		void call_back(const std_msgs::String::ConstPtr& msg);
+    // function
+    void call_back(const std_msgs::String::ConstPtr &msg);
 };
 
-void Listener::call_back(const std_msgs::String::ConstPtr& msg)
-{
-	this->receive_data = msg->data;
-	ROS_INFO("Received data : %s", this->receive_data.c_str());
+void Listener::call_back(const std_msgs::String::ConstPtr &msg) {
+    this->receive_data = msg->data;
+    ROS_INFO_STREAM("Received data : " << this->receive_data);
 }
 
-int main(int argc, char* argv[])
-{
-	ros::init(argc, argv, "listener_class");
-	ros::NodeHandle node_handle;
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "listener_class");
+    ros::NodeHandle node_handle;
 
-	ros::Rate rate(1);
+    ros::Rate rate(1);
 
-	Listener listener;
-	ros::Subscriber sub = node_handle.subscribe("speek", 10, &Listener::call_back, &listener);
+    Listener listener;
+    ros::Subscriber sub =
+        node_handle.subscribe("speek", 10, &Listener::call_back, &listener);
 
-	while (ros::ok()) {
-		ros::spinOnce();
-		rate.sleep();
-	}
-	
-	return 0;
+    while (ros::ok()) {
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
 }
